465b07a14d38dc7f797f008f8a8fa8cbd3e3d1e3
Arvind <thakurarvindkr10@gmail.com>
2023-05-16 17:31:43 +0530

    Navbar and Hero part is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
-import Header from './components/Header/Header'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Store from './components/Pages/Store'
-import Home from './components/Pages/Home'
-import About from './components/Pages/About'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' exact element={<Home />}></Route>
-        <Route path='/store' exact element={<Store />}></Route>
-        <Route path='/about' exact element={<About />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Navbar />
+      <Hero/>
+    </div>
   )
 }
 
