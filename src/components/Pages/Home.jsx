0d29ddcc1893ac4c78f2be45ae1eaff5f2e598b8
Arvind <thakurarvindkr10@gmail.com>
2023-06-14 02:05:22 +0530

    implement home page

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
-
+import { AiOutlinePlayCircle } from 'react-icons/ai'
 const Home = () => {
     return (
-        <h1></h1>
+        <div>
+            <div className='bg-[#777]'>
+                <p className='text-center text-5xl pt-1 pb-8 font-semibold text-yellow-100'>The Generics</p>
+                <div className='text-center'>
+                    <div>
+                        <button className='text-2xl text-yellow-100 border p-2 border-sky-400'>Get our Latest Album</button>
+                    </div>
+                    <div className='inline-block m-6'>
+                        <AiOutlinePlayCircle className='text-6xl text-sky-400' />
+                    </div>
+                </div>
+            </div>
+            <p className='text-center font-mono text-2xl m-3'>TOURS</p>
+            <div className='text-center'>
+                <ul className=''>
+                    <div className='flex justify-center items-center gap-5 m-2'>
+                        <li className='border-b-2 text-xl'>JUL16</li>
+                        <li className='border-b-2 text-sm'>DETROIT, MI</li>
+                        <li className='border-b-2 text-sm'>DTE ENERGY MUSIC THEATRE</li>
+                        <li className='border-b-2'><brutton className="bg-blue-400 px-2 text-white rounded-lg">BUY TICKETS</brutton></li>
+                    </div>
+                    <div className='flex justify-center items-center gap-5 m-2'>
+                        <li className='border-b-2 text-xl'>JUL16</li>
+                        <li className='border-b-2 text-sm'>DETROIT, MI</li>
+                        <li className='border-b-2 text-sm'>DTE ENERGY MUSIC THEATRE</li>
+                        <li className='border-b-2'><brutton className="bg-blue-400 px-2 text-white rounded-lg">BUY TICKETS</brutton></li>
+                    </div>
+                    <div className='flex justify-center items-center gap-5 m-2'>
+                        <li className='border-b-2 text-xl'>JUL16</li>
+                        <li className='border-b-2 text-sm'>DETROIT, MI</li>
+                        <li className='border-b-2 text-sm'>DTE ENERGY MUSIC THEATRE</li>
+                        <li className='border-b-2'><brutton className="bg-blue-400 px-2 text-white rounded-lg">BUY TICKETS</brutton></li>
+                    </div>
+                    <div className='flex justify-center items-center gap-5 m-2'>
+                        <li className='border-b-2 text-xl'>JUL16</li>
+                        <li className='border-b-2 text-sm'>DETROIT, MI</li>
+                        <li className='border-b-2 text-sm'>DTE ENERGY MUSIC THEATRE</li>
+                        <li className='border-b-2'><brutton className="bg-blue-400 px-2 text-white rounded-lg">BUY TICKETS</brutton></li>
+                    </div>
+                    <div className='flex justify-center items-center gap-5 m-2'>
+                        <li className='border-b-2 text-xl'>JUL16</li>
+                        <li className='border-b-2 text-sm'>DETROIT, MI</li>
+                        <li className='border-b-2 text-sm'>DTE ENERGY MUSIC THEATRE</li>
+                        <li className='border-b-2'><brutton className="bg-blue-400 px-2 text-white rounded-lg">BUY TICKETS</brutton></li>
+                    </div>
+                    <div className='flex justify-center items-center gap-5 m-2'>
+                        <li className='border-b-2 text-xl'>JUL16</li>
+                        <li className='border-b-2 text-sm'>DETROIT, MI</li>
+                        <li className='border-b-2 text-sm'>DTE ENERGY MUSIC THEATRE</li>
+                        <li className='border-b-2'><brutton className="bg-blue-400 px-2 text-white rounded-lg">BUY TICKETS</brutton></li>
+                    </div>
+                    <div className='flex justify-center items-center gap-5 m-2'>
+                        <li className='border-b-2 text-xl'>JUL16</li>
+                        <li className='border-b-2 text-sm'>DETROIT, MI</li>
+                        <li className='border-b-2 text-sm'>DTE ENERGY MUSIC THEATRE</li>
+                        <li className='border-b-2'><brutton className="bg-blue-400 px-2 text-white rounded-lg">BUY TICKETS</brutton></li>
+                    </div>
+                    <div className='flex justify-center items-center gap-5 m-2'>
+                        <li className='border-b-2 text-xl'>JUL16</li>
+                        <li className='border-b-2 text-sm'>DETROIT, MI</li>
+                        <li className='border-b-2 text-sm'>DTE ENERGY MUSIC THEATRE</li>
+                        <li className='border-b-2'><brutton className="bg-blue-400 px-2 text-white rounded-lg">BUY TICKETS</brutton></li>
+                    </div>
+                    <div className='flex justify-center items-center gap-5 m-2'>
+                        <li className='border-b-2 text-xl'>JUL16</li>
+                        <li className='border-b-2 text-sm'>DETROIT, MI</li>
+                        <li className='border-b-2 text-sm'>DTE ENERGY MUSIC THEATRE</li>
+                        <li className='border-b-2'><brutton className="bg-blue-400 px-2 text-white rounded-lg">BUY TICKETS</brutton></li>
+                    </div>
+                </ul>
+            </div>
+        </div>
     )
+
 }
 
 export default Home
